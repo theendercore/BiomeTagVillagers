e484d185d305c0cd13146ecd1898cbfb357960ae
TheEnderCore <theendercore@gmail.com>
2025-04-29 12:18:44 +0300

    - Replaced Inject with ModifyReturnValue - re-wrote fetching code to have fallback for modded villager types

--- src/main/java/com/theendercore/biome_tag_villagers/mixin/VillagerTypeMixer.java
+++ src/main/java/com/theendercore/biome_tag_villagers/mixin/VillagerTypeMixer.java
@@ -1,34 +1,42 @@
 package com.theendercore.biome_tag_villagers.mixin;
 
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import net.minecraft.registry.Registries;
 import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.village.VillagerType;
 import net.minecraft.world.biome.Biome;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.List;
 import java.util.Map;
 
 import static com.theendercore.biome_tag_villagers.BiomeTagVillagers.getBiomeTagToType;
-import static net.minecraft.village.VillagerType.PLAINS;
 
 @Mixin(VillagerType.class)
 public class VillagerTypeMixer {
 
-	@Unique
-	private final static Random BiomeTag$random = Random.create();
+    @Shadow
+    @Final
+    public static VillagerType PLAINS;
+    @Unique
+    private final static Random biome_tag$random = Random.create();
 
-	@Inject(at = @At("RETURN"), method = "forBiome", cancellable = true)
-	private static void typeFromBiomeTag(RegistryEntry<Biome> biomeEntry, CallbackInfoReturnable<VillagerType> cir) {
-		List<VillagerType> types = getBiomeTagToType().entrySet().stream()
-				.filter(pair -> biomeEntry.isIn(pair.getKey()))
-				.map(Map.Entry::getValue)
-				.toList();
+    @ModifyReturnValue(method = "forBiome", at = @At("RETURN"))
+    private static VillagerType typeFromBiomeTag(VillagerType original, RegistryEntry<Biome> biomeEntry) {
+        List<VillagerType> types = getBiomeTagToType().entrySet().stream()
+                .filter(pair -> biomeEntry.isIn(pair.getKey()))
+                .map(Map.Entry::getValue)
+                .toList();
 
-		cir.setReturnValue(types.isEmpty() ? PLAINS : types.get(BiomeTag$random.nextBetween(0, types.size() - 1)));
-	}
+        if (!types.isEmpty()) {
+            return types.get(biome_tag$random.nextBetween(0, types.size() - 1));
+        }
+        if (Registries.VILLAGER_TYPE.getId(original).getNamespace().equals(Identifier.DEFAULT_NAMESPACE)) {
+            return PLAINS;
+        }
+        return original;
+    }
 }
