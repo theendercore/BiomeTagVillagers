9875ec1a64ebbbccaef83654843b757a56dfff87
TheEnderCore <theendercore@gmail.com>
2025-04-29 12:16:01 +0300

    - of -> key

--- src/main/java/com/theendercore/biome_tag_villagers/BiomeTagVillagers.java
+++ src/main/java/com/theendercore/biome_tag_villagers/BiomeTagVillagers.java
@@ -2,7 +2,6 @@ package com.theendercore.biome_tag_villagers;
 
 import com.google.common.collect.Maps;
 import net.fabricmc.api.ModInitializer;
-
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.tag.TagKey;
 import net.minecraft.util.Identifier;
@@ -19,21 +18,20 @@ import static net.minecraft.village.VillagerType.*;
 public class BiomeTagVillagers implements ModInitializer {
     public static final String MODID = "biome_tag_villagers";
     public static final Logger log = LoggerFactory.getLogger(MODID);
-    public static final TagKey<Biome> VILLAGER_DESERT = of("villager_desert");
-    public static final TagKey<Biome> VILLAGER_JUNGLE = of("villager_jungle");
-    public static final TagKey<Biome> VILLAGER_SAVANNA = of("villager_savanna");
-    public static final TagKey<Biome> VILLAGER_SNOWY = of("villager_snowy");
-    public static final TagKey<Biome> VILLAGER_SWAMP = of("villager_swamp");
-    public static final TagKey<Biome> VILLAGER_TAIGA = of("villager_taiga");
-    private static final Map<TagKey<Biome>, VillagerType> BIOME_TAG_TO_TYPE =
-            Util.make(Maps.newHashMap(), map -> {
-                map.put(VILLAGER_DESERT, DESERT);
-                map.put(VILLAGER_JUNGLE, JUNGLE);
-                map.put(VILLAGER_SAVANNA, SAVANNA);
-                map.put(VILLAGER_SNOWY, SNOW);
-                map.put(VILLAGER_SWAMP, SWAMP);
-                map.put(VILLAGER_TAIGA, TAIGA);
-            });
+    public static final TagKey<Biome> VILLAGER_DESERT = key("villager_desert");
+    public static final TagKey<Biome> VILLAGER_JUNGLE = key("villager_jungle");
+    public static final TagKey<Biome> VILLAGER_SAVANNA = key("villager_savanna");
+    public static final TagKey<Biome> VILLAGER_SNOWY = key("villager_snowy");
+    public static final TagKey<Biome> VILLAGER_SWAMP = key("villager_swamp");
+    public static final TagKey<Biome> VILLAGER_TAIGA = key("villager_taiga");
+    private static final Map<TagKey<Biome>, VillagerType> BIOME_TAG_TO_TYPE = Util.make(Maps.newHashMap(), map -> {
+        map.put(VILLAGER_DESERT, DESERT);
+        map.put(VILLAGER_JUNGLE, JUNGLE);
+        map.put(VILLAGER_SAVANNA, SAVANNA);
+        map.put(VILLAGER_SNOWY, SNOW);
+        map.put(VILLAGER_SWAMP, SWAMP);
+        map.put(VILLAGER_TAIGA, TAIGA);
+    });
 
     @Override
     public void onInitialize() {
@@ -49,7 +47,7 @@ public class BiomeTagVillagers implements ModInitializer {
         return BIOME_TAG_TO_TYPE;
     }
 
-    private static TagKey<Biome> of(String id) {
+    private static TagKey<Biome> key(String id) {
         return TagKey.of(RegistryKeys.BIOME, Identifier.of(MODID, id));
     }
 }
