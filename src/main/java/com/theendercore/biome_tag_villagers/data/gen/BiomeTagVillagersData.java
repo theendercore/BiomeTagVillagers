cd1f3307ecb22d7ec2ea6a88ea1bdd3f7b8f7b04
TheEnderCore <theendercore@gmail.com>
2025-04-29 12:16:16 +0300

    - fixed bad whitespace

--- src/main/java/com/theendercore/biome_tag_villagers/data/gen/BiomeTagVillagersData.java
+++ src/main/java/com/theendercore/biome_tag_villagers/data/gen/BiomeTagVillagersData.java
@@ -16,36 +16,35 @@ import static com.theendercore.biome_tag_villagers.BiomeTagVillagers.log;
 
 @SuppressWarnings("unused")
 public class BiomeTagVillagersData implements DataGeneratorEntrypoint {
-	@Override
-	public void onInitializeDataGenerator(FabricDataGenerator gen) {
-		log.info("Hello from DataGen");
-		var pack = gen.createPack();
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator gen) {
+        log.info("Hello from DataGen");
+        var pack = gen.createPack();
 
-		pack.addProvider(BiomeTagProvider::new);
-	}
+        pack.addProvider(BiomeTagProvider::new);
+    }
 
-	static class BiomeTagProvider extends FabricTagProvider<Biome> {
-		public BiomeTagProvider(FabricDataOutput o, CompletableFuture<RegistryWrapper.WrapperLookup> r) {
-			super(o, RegistryKeys.BIOME, r);
-		}
+    static class BiomeTagProvider extends FabricTagProvider<Biome> {
+        public BiomeTagProvider(FabricDataOutput o, CompletableFuture<RegistryWrapper.WrapperLookup> r) {
+            super(o, RegistryKeys.BIOME, r);
+        }
 
-		@Override
-		protected void configure(RegistryWrapper.WrapperLookup wrapperLookup) {
-			getOrCreateTagBuilder(BiomeTagVillagers.VILLAGER_DESERT)
-					.forceAddTag(ConventionalBiomeTags.IS_DESERT)
-					.forceAddTag(ConventionalBiomeTags.IS_BADLANDS);
-			getOrCreateTagBuilder(BiomeTagVillagers.VILLAGER_JUNGLE)
-					.forceAddTag(ConventionalBiomeTags.IS_JUNGLE);
-			getOrCreateTagBuilder(BiomeTagVillagers.VILLAGER_SAVANNA)
-					.forceAddTag(ConventionalBiomeTags.IS_SAVANNA);
-			getOrCreateTagBuilder(BiomeTagVillagers.VILLAGER_SNOWY)
-					.forceAddTag(ConventionalBiomeTags.IS_SNOWY)
-					.forceAddTag(ConventionalBiomeTags.IS_AQUATIC_ICY);
-			getOrCreateTagBuilder(BiomeTagVillagers.VILLAGER_SWAMP)
-					.forceAddTag(ConventionalBiomeTags.IS_SWAMP);
-			getOrCreateTagBuilder(BiomeTagVillagers.VILLAGER_TAIGA)
-					.forceAddTag(ConventionalBiomeTags.IS_TAIGA);
-
-		}
-	}
+        @Override
+        protected void configure(RegistryWrapper.WrapperLookup wrapperLookup) {
+            getOrCreateTagBuilder(BiomeTagVillagers.VILLAGER_DESERT)
+                    .forceAddTag(ConventionalBiomeTags.IS_DESERT)
+                    .forceAddTag(ConventionalBiomeTags.IS_BADLANDS);
+            getOrCreateTagBuilder(BiomeTagVillagers.VILLAGER_JUNGLE)
+                    .forceAddTag(ConventionalBiomeTags.IS_JUNGLE);
+            getOrCreateTagBuilder(BiomeTagVillagers.VILLAGER_SAVANNA)
+                    .forceAddTag(ConventionalBiomeTags.IS_SAVANNA);
+            getOrCreateTagBuilder(BiomeTagVillagers.VILLAGER_SNOWY)
+                    .forceAddTag(ConventionalBiomeTags.IS_SNOWY)
+                    .forceAddTag(ConventionalBiomeTags.IS_AQUATIC_ICY);
+            getOrCreateTagBuilder(BiomeTagVillagers.VILLAGER_SWAMP)
+                    .forceAddTag(ConventionalBiomeTags.IS_SWAMP);
+            getOrCreateTagBuilder(BiomeTagVillagers.VILLAGER_TAIGA)
+                    .forceAddTag(ConventionalBiomeTags.IS_TAIGA);
+        }
+    }
 }
